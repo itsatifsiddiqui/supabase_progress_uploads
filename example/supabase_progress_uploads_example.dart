8a2fa65ed04ea87e58418b46aedbbc203eaed857
Atif Siddiqui <itsatifsiddiqui@gmail.com>
2024-07-18 13:23:56 +0500

    Initial commit for supabase_progress_uploads package

---
+++ example/supabase_progress_uploads_example.dart
@@ -0,0 +1,6 @@
+import 'package:supabase_progress_uploads/supabase_progress_uploads.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/supabase_progress_uploads_test.dart
@@ -0,0 +1,16 @@
+import 'package:supabase_progress_uploads/supabase_progress_uploads.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
